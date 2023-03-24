2ed2160bf15429384dd3332c9135bfe82df01923
AlbrightSunguti <albright.sunguti@abxtractventurelabs.com>
2023-03-24 11:21:42 +0300

    fix/fix route redirect to courses page

--- client/src/app/modules/courses/courses-routing.module.ts
+++ client/src/app/modules/courses/courses-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './courses.component';
 
-const routes: Routes = [];
+const routes : Routes = [
+  {
+    path:'',
+    // component: CoursesComponent,
+    children:[
+      {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+      },
+      {
+        path:'home',
+        component: HomeComponent
+      }
+    ]
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
